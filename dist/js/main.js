68d29071f8d394a491061dca8ce8cf5e8cc7f6b5
unknown <shivani.vd15595@gmail.com>
2019-08-28 12:23:06 +0530

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,38 @@
+//only first item selected
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+//All will select All items with that id/class name.
+const navItems=document.querySelectorAll('.nav-item');
+
+let showMenu=false;
+menuBtn.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+	if(!showMenu){
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItems.forEach((navItem)=>{
+			navItem.classList.add('show')
+		});
+		showMenu=true;
+	}
+	else{
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItems.forEach((navItem)=>{
+			navItem.classList.remove('show')
+		});
+		showMenu=false;
+
+	}
+}
+
+
